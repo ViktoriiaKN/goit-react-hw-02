3dd5cacc33ec3eeba205c2bf163f5e8d614dd165
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-07-06 09:32:38 +0200

    Task 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Description from './components/Description/Description';
 import Feedback from './components/Feedback/Feedback';
@@ -6,32 +6,47 @@ import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
 
 function App() {
-  const [feedbacks, setFeedbacks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedbacks, setFeedbacks] = useState(() => {
+    const saveFeedbacks = window.localStorage.getItem('saved-feedbacks');
+    return saveFeedbacks
+      ? JSON.parse(saveFeedbacks)
+      : { good: 0, neutral: 0, bad: 0 };
   });
 
-  const updateFeedback = feedbackType => {
-setFeedbacks(prevFeedbacks =>  ({...prevFeedbacks, [feedbackType]: prevFeedbacks[feedbackType] + 1}
-))
-  }
+  useEffect(() => {
+    window.localStorage.setItem('saved-feedbacks', JSON.stringify(feedbacks));
+  }, [feedbacks]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedbacks((prevFeedbacks) => ({
+      ...prevFeedbacks,
+      [feedbackType]: prevFeedbacks[feedbackType] + 1,
+    }));
+  };
 
   const resetFeedbacks = () => {
     setFeedbacks({
-      good: 0, 
+      good: 0,
       neutral: 0,
       bad: 0,
-    })
-  }
+    });
+  };
 
- const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
+  const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
 
   return (
     <>
       <Description />
-      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} resetFeedbacks={resetFeedbacks}/>
-      {totalFeedback > 0 ? (<Feedback feedbacks={feedbacks} totalFeedback={totalFeedback}/>) : (<Notification/>) }
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedbacks={resetFeedbacks}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback feedbacks={feedbacks} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification />
+      )}
     </>
   );
 }
